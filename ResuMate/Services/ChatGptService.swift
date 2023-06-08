704fb7ce8c22e5d98fd73dd6689b99a482ac58c5
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-06-08 21:16:57 +0700

    add relevant content checking API

--- ResuMate/Services/ChatGptService.swift
+++ ResuMate/Services/ChatGptService.swift
@@ -187,6 +187,80 @@ class ChatGptService {
             }
         }
     }
+    
+    func fetchRelevancyScoreByJobTitle(jobTitle: String, content: String, completion: @escaping (Result<[RelevancyScoreModel], Error>) -> Void) {
+        let prompt = """
+        \(content)
+        
+        rate from 0 to 100 if the text above is relevant to \(jobTitle) in a json array format with the only field name 'score'
+        """
+        
+        self.fetchChatGptApi(prompt: prompt) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    print("[isContentRelevantByJobTitle][fetchChatGptApi]")
+                    
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                    print("[isContentRelevantByJobTitle][fetchChatGptApi][json]", json as Any)
+                    
+                    // Check if the JSON response contains an 'error' key.
+                    if let error = json?["error"] as? [String: Any] {
+                        let errorMessage = error["message"] as? String ?? "Unknown error occurred"
+                        let error = NSError(domain: "ChatGPT API", code: 0, userInfo: [NSLocalizedDescriptionKey: errorMessage])
+                        print(error)
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    if let choices = json?["choices"] as? [[String: Any]],
+                       var text = choices.first?["text"] as? String {
+                        print("[isContentRelevantByJobTitle][fetchChatGptApi][choices]", choices)
+                        print("[isContentRelevantByJobTitle][fetchChatGptApi][text]", text)
+                        
+                        // Check if the last character is a closing bracket
+                        if !text.hasSuffix("]") {
+                            // Find the index of the last complete suggestion
+                            if let lastIndex = text.lastIndex(of: "}") {
+                                // Trim the text to include only complete suggestions and append a closing bracket
+                                let endIndex = text.index(after: lastIndex)
+                                text = String(text[..<endIndex])
+                                text.append("]")
+                            }
+                        }
+                        
+                        // Convert the text back to Data for parsing
+                        if let jsonData = text.data(using: .utf8) {
+                            do {
+                                // Decode the data to an array of SuggestionModel
+                                let relevancyScore = try JSONDecoder().decode([RelevancyScoreModel].self, from: jsonData)
+                                completion(.success(relevancyScore))
+                            } catch {
+                                print(error)
+                                completion(.failure(error))
+                            }
+                        } else {
+                            let error = NSError(domain: "BiodataModel Decoding", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to convert completed text to data"])
+                            print(error)
+                            completion(.failure(error))
+                        }
+                    } else {
+                        let error = NSError(domain: "JSON Parsing", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to parse JSON data"])
+                        print(error)
+                        completion(.failure(error))
+                    }
+
+                } catch {
+                    completion(.failure(error))
+                }
+                break
+            case .failure(let error):
+                print(error)
+                completion(.failure(error))
+                break
+            }
+        }
+    }
 
     //MARK: You can call function on this file!
     //MARK: Function to get biodata!
